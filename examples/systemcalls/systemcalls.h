b556ac45ba473dc8679e23a915fd28737ef0c80d
Harshal Wadhwa <hawa7555@colorado.edu>
2025-09-11 22:10:47 -0600

    Assignment-3-Part-1

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 bool do_system(const char *command);
 
 bool do_exec(int count, ...);
 
 bool do_exec_redirect(const char *outputfile, int count, ...);
+
